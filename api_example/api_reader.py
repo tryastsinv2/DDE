af75463e586df80388645361e16d9d6ccab00a4a
tryastsinv2 <tryastsinv2@gmail.com>
2025-10-14 19:39:06 +0300

    исправил лабораторную 4

--- api_example/api_reader.py
+++ api_example/api_reader.py
@@ -5,6 +5,14 @@ url = "http://universities.hipolabs.com/search?country=Kuwait"
 headers = {"Content-Type": "application/json"}
 
 response = requests.get(url)
-data = response.json()
-df = pd.DataFrame(data)
-print(df)
+if response.status_code == 200:
+    try:
+        data = response.json()
+        df = pd.DataFrame(data)
+        df.dropna()
+        print(df.head(5))
+        df.to_csv("api_reader_result.csv")
+    except requests.RequestException:
+        print("Ошибка при обработке запроса")
+else:
+    print("API не доступно")
